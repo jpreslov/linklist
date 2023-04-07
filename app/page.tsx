f0b30739e1fc221f15a25178f4e880a3c4cb39ea
Jake Lovett <lovett_jp@outlook.com>
2023-04-07 12:07:01 -0500

    2nd

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 const notoSans = Noto_Sans({
   subsets: ['latin'],
-  weight: '900'
+  weight: '900',
 })
 
 export default function Home() {
@@ -29,19 +29,45 @@ export default function Home() {
   ]
 
   return (
-    <main className='w-screen h-screen flex flex-col bg-indigo-950 justify-center align-middle content-center'>
-      {/* <div className='flex flex-col'>
-        <h1 className={notoSans.className}>anosmia</h1>
-      </div> */}
-      {
-        links.map((link, id) => {
-          return (
-            <div key={id} className='flex m-3'>
-              <LinkBtn name={link.name} url={link.url} />
-            </div>
-          )
-        })
-      }
+    <main
+      className='w-screen h-screen flex flex-col bg-indigo-950 justify-center align-middle content-center'
+    >
+      <div className='inline w-screen top-0 fixed h-[6em] text-center'>
+        <h1
+          className={notoSans.className}
+          style={{
+            fontSize: '90px',
+            letterSpacing: '-11px',
+            color: 'white',
+            backgroundBlendMode: 'screen'
+          }}
+        >
+          anosmia
+        </h1>
+      </div>
+      <div
+        className='w-screen flex flex-row h-[70px] justify-center m-5 text-center'
+      >
+        <p
+          className='w-screen text-4xl'
+          style={{ transform: '1s' }}
+        >
+          👾
+        </p>
+      </div>
+      <div className='flex justify-center align-middle h-1/2'>
+        <div className='flex flex-col p-4 justify-center w-3/4 max-w-[700px] border-white rounded-lg border-2'>
+          {
+            links.map((link, id) => {
+              return (
+                <div key={id} className='flex m-3'>
+                  <LinkBtn name={link.name} url={link.url} />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </main>
   )
 }
